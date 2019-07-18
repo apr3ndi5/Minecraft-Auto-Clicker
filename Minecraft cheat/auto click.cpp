7b234d9b24e7025ae22892a4f0c70e8f0910c8b1
Guilherme Lima Araujo <38929104+apr3ndi5@users.noreply.github.com>
2019-07-18 06:02:54 -0300

    verificação de janela

--- Minecraft cheat/auto click.cpp
+++ Minecraft cheat/auto click.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 #include <Windows.h>
+#include <string>
+using std::string;
 using namespace std;
 
-int main() {
+int main() 
+{
 
+string GetActiveWindowTitle()
+{
+ char wnd_title[256];
+ HWND hwnd=GetForegroundWindow(); 
+ GetWindowText(hwnd,wnd_title,sizeof(wnd_title));
+ return wnd_title;
+}
 	DWORD NumToSleep = 1000;
 	int TimeToSleep = 1000;
 	bool click = false;
@@ -17,22 +27,28 @@ int main() {
 	cout << endl << "Aperte DOWN para diminuir o delay" << endl;
 	cout << "----------------------------------" << endl;
 
-	while (1) {
+	while (1 || GetActiveWindowTitle = "Minecraft 1.14") 
+	{
 
-		if (GetAsyncKeyState('F')) {
+		if (GetAsyncKeyState('F')) 
+		{
 			click = true;
 		}
-		if (GetAsyncKeyState('G')) {
+		else if (GetAsyncKeyState('G'))
+		{
 			click = false;
 		}
-		if (click == false) {
+		else if (click == false)
+		{
 			mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
 		}
-		if (GetAsyncKeyState(VK_UP) & 0) {
+		else if (GetAsyncKeyState(VK_UP) & 0) 
+		{
 			NumToSleep += TimeToSleep;
 			cout << "Valor do delay: " << NumToSleep;
 		}
-		if (GetAsyncKeyState(VK_DOWN) & 0) {
+		else if (GetAsyncKeyState(VK_DOWN) & 0)
+		{
 			NumToSleep -= TimeToSleep;
 			cout << "Valor do delay: " << NumToSleep;
 		}
@@ -40,6 +56,6 @@ int main() {
 		Sleep(NumToSleep);
 	}
 
-	system("PAUSE");
+	system("pause");
 	return 0;
 }
